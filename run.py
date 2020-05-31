af4b925000008cbdf04b18f6335b275250033a96
dfesenko5 <fesenkogithub@gmail.com>
2020-05-31 17:36:05 +0300

    Refactor: implemented the write_to_csv() function and take out the corresponding logic from the main() function.

--- run.py
+++ run.py
@@ -19,72 +19,71 @@ def main(driver_path):
 
     count_scrapped = 0
 
-    with open('jobs2.csv', mode='w') as csv_file:
-        fieldnames = ['category', 'title', 'company', 'location', 'date', 'url']
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-        writer.writeheader()
+    write_to_csv(is_headline=True)
 
-        for i in range(len(links_to_categories)):
-            category_name = category_names[i]
-            print(category_name)
-            driver.get(links_to_categories[i])
-            time.sleep(1)
+    for i in range(len(links_to_categories)):
+        category = category_names[i]
+        print(category)
+        driver.get(links_to_categories[i])
+        time.sleep(1)
 
-            more_btn = driver.find_element_by_xpath("//div[@class='more-btn']/a")
-            while True:
-                try:
-                    more_btn.click()
-                    time.sleep(2)
-                except ElementNotInteractableException:
-                    break
+        more_btn = driver.find_element_by_xpath("//div[@class='more-btn']/a")
+        while True:
+            try:
+                more_btn.click()
+                time.sleep(2)
+            except ElementNotInteractableException:
+                break
 
-            vacancies = driver.find_elements_by_xpath("//div[@class='vacancy']/div[@class='title']/a[@class='vt']")
+        vacancies = driver.find_elements_by_xpath("//div[@class='vacancy']/div[@class='title']/a[@class='vt']")
 
-            print(f"For {category_name} detected {len(vacancies)} vacancies.")
+        print(f"For {category} detected {len(vacancies)} vacancies.")
 
-            links_to_vacancies = [vacancy.get_attribute('href') for vacancy in vacancies]
-            vacancy_titles = [vacancy.text for vacancy in vacancies]
+        links_to_vacancies = [vacancy.get_attribute('href') for vacancy in vacancies]
+        vacancy_titles = [vacancy.text for vacancy in vacancies]
 
-            for j in range(len(links_to_vacancies)):
-                title = vacancy_titles[j]
-                driver.get(links_to_vacancies[j])
-                time.sleep(5)
+        for j in range(len(links_to_vacancies)):
+            title = vacancy_titles[j]
+            driver.get(links_to_vacancies[j])
+            time.sleep(3)
 
-                company = None
-                location = None
-                date = None
+            company = None
+            location = None
+            date = None
 
-                try:
-                    company = driver.find_elements_by_xpath(
-                        "//div[@class='b-vacancy']/div[@class='b-compinfo']/div[@class='info']//a[1]")[0].text
-                    location = driver.find_element_by_xpath("//div[@class='b-vacancy']//span[@class='place']").text
-                    date = driver.find_element_by_xpath("//div[@class='b-vacancy']//div[@class='date']").text
-                except NoSuchElementException:
-                    pass
+            try:
+                company = driver.find_elements_by_xpath(
+                    "//div[@class='b-vacancy']/div[@class='b-compinfo']/div[@class='info']//a[1]")[0].text
+                location = driver.find_element_by_xpath("//div[@class='b-vacancy']//span[@class='place']").text
+                date = driver.find_element_by_xpath("//div[@class='b-vacancy']//div[@class='date']").text
+            except NoSuchElementException:
+                pass
 
-                url = driver.current_url
+            url = driver.current_url
 
-                writer.writerow({'category': category_name, 'title': title, 'company': company,
-                                 'location': location, 'date': date, 'url': url})
+            write_to_csv(category=category, title=title, company=company, location=location, date=date, url=url)
 
-                count_scrapped += 1
+            count_scrapped += 1
 
-                if count_scrapped % 10 == 0:
-                    print(count_scrapped)
+            if count_scrapped % 20 == 0:
+                print(count_scrapped)
 
-            print(f"Scrapped category {category_name}")
-            time.sleep(10)
+        print(f"Scrapped category {category}")
+        time.sleep(5)
 
     driver.close()
 
 
+def write_to_csv(is_headline=False, category=None, title=None, company=None, location=None, date=None, url=None):
+    fieldnames = ['category', 'title', 'company', 'location', 'date', 'url']
+    with open('jobs2.csv', mode='a+') as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        if is_headline:
+            writer.writeheader()
+        else:
+            writer.writerow({'category': category, 'title': title, 'company': company,
+                             'location': location, 'date': date, 'url': url})
+
+
 if __name__ == '__main__':
     main(DRIVER_PATH)
-
-
-
-
-
-
-
-
